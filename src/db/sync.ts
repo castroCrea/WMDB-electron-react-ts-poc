eb221e421533c83a5681f307263b6d18b5f54b7c
castroCrea <castro.crea@gmail.com>
2020-12-17 13:05:21 +0100

    🎨

--- src/db/sync.ts
+++ src/db/sync.ts
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-anonymous-default-export */
 import { synchronize } from '@nozbe/watermelondb/sync'
 import { database } from './connectDb'
 
@@ -9,36 +10,36 @@ export default async (): Promise<void> => {
   console.log(process.env)
   console.log(`${url}/api/sync/watermelon`)
 
-  // await synchronize({
-  //   database,
-  //   pullChanges: async ({ lastPulledAt, schemaVersion, migration }) => {
-  //     const response = await fetch(`${url}/api/sync/watermelon`, {
-  //       body: JSON.stringify({ lastPulledAt, schemaVersion, migration }),
-  //       headers: {
-  //         Authorization: bearer,
-  //         'Content-Type': 'application/json'
-  //       }
-  //     })
-  //     if (!response.ok) {
-  //       throw new Error(await response.text())
-  //     }
+  await synchronize({
+    database,
+    pullChanges: async ({ lastPulledAt, schemaVersion, migration }) => {
+      const response = await fetch(`${url}/api/sync/watermelon`, {
+        body: JSON.stringify({ lastPulledAt, schemaVersion, migration }),
+        headers: {
+          Authorization: bearer,
+          'Content-Type': 'application/json'
+        }
+      })
+      if (!response.ok) {
+        throw new Error(await response.text())
+      }
 
-  //     const { changes, timestamp } = await response.json()
-  //     return { changes, timestamp }
-  //   },
-  //   pushChanges: async ({ changes, lastPulledAt }) => {
-  //     const response = await fetch(`https://my.backend/sync?last_pulled_at=${lastPulledAt}`, {
-  //       method: 'POST',
-  //       body: JSON.stringify(changes),
-  //       headers: {
-  //         Authorization: bearer,
-  //         'Content-Type': 'application/json'
-  //       }
-  //     })
-  //     if (!response.ok) {
-  //       throw new Error(await response.text())
-  //     }
-  //   },
-  //   migrationsEnabledAtVersion: 1
-  // })
+      const { changes, timestamp } = await response.json()
+      return { changes, timestamp }
+    },
+    pushChanges: async ({ changes, lastPulledAt }) => {
+      const response = await fetch(`https://my.backend/sync?last_pulled_at=${lastPulledAt}`, {
+        method: 'POST',
+        body: JSON.stringify(changes),
+        headers: {
+          Authorization: bearer,
+          'Content-Type': 'application/json'
+        }
+      })
+      if (!response.ok) {
+        throw new Error(await response.text())
+      }
+    },
+    migrationsEnabledAtVersion: 1
+  })
 }
